cb2e2501681d1537b38326fff77bbe8e9da8f012
David Hou <david.hou314@gmail.com>
2013-05-18 19:33:11 -0700

    ukg

--- ukg.mk
+++ ukg.mk
@@ -2,16 +2,16 @@
 $(call inherit-product, device/samsung/d2att/full_d2att.mk)
 
 # Inherit common product files.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/ukg/config/common.mk)
 
 # Inherit CDMA common stuff
-$(call inherit-product, vendor/aosp/config/gsm.mk)
+$(call inherit-product, vendor/ukg/config/gsm.mk)
 
 # d2att Overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/aosp/overlay/toro
+PRODUCT_PACKAGE_OVERLAYS += vendor/ukg/overlay/toro
 
 # Setup device specific product configuration.
-PRODUCT_NAME := xylon_d2att
+PRODUCT_NAME := ukg_d2att
 PRODUCT_BRAND := Samsung
 PRODUCT_DEVICE := d2att
 PRODUCT_MODEL := SGH-I747
@@ -21,7 +21,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2att TARGET_DEVICE=d2att BUILD_FIN
 
 # Copy mako specific prebuilt files
 PRODUCT_COPY_FILES +=  \
-    vendor/aosp/prebuilt/hybrid_xhdpi_no_nav.conf:system/etc/beerbong/properties.conf \
-    vendor/aosp/prebuilt/bootanimation/bootanimation_1280_720.zip:system/media/bootanimation.zip \
-    vendor/aosp/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
-    vendor/aosp/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd 
+    vendor/ukg/prebuilt/hybrid_xhdpi_no_nav.conf:system/etc/beerbong/properties.conf \
+    vendor/ukg/prebuilt/bootanimation/bootanimation_1280_720.zip:system/media/bootanimation.zip \
+    vendor/ukg/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/ukg/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd 
