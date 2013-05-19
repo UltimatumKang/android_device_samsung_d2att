3b49337d09da35d82a8f7b275d35a9d9a37ccd81
David Hou <david.hou314@gmail.com>
2013-05-18 20:35:34 -0700

    Damn...

---
+++ AndroidProducts.mk
@@ -0,0 +1,1 @@
+PRODUCT_MAKEFILES += device/samsung/d2att/full_d2att.mk
--- full_d2att.mk
+++ full_d2att.mk
@@ -27,7 +27,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/d2att/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := ukg_d2att
+PRODUCT_NAME := full_d2att
 PRODUCT_DEVICE := d2att
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
